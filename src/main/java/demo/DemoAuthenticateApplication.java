c042219efb07ebd4db1e2feb2477414339c09333
Emerson Melo <emerson_sardinha@hotmail.com>
2021-05-20 16:16:55 -0300

    Projeto criado com estrutura de pacotes inicial

---
+++ src/main/java/demo/DemoAuthenticateApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthenticateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthenticateApplication.class, args);
+	}
+
+}
