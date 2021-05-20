2d7c71c3ad386e9afdaf577b9bbc79fae662dfa3
Emerson Melo <emerson_sardinha@hotmail.com>
2021-05-20 16:44:13 -0300

    Testando primeira classe de dominio

--- src/main/java/demo/resources/UsuarioResource.java
+++ src/main/java/demo/resources/UsuarioResource.java
@@ -1,15 +1,29 @@
 package demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import demo.domain.Usuario;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Usuario> listar() {
+		
+		Usuario usuario1 = new Usuario(1l, "Emerson", "Emerson@sardinha", "123");
+		Usuario usuario2 = new Usuario(2l, "Joanete", "Joanete@silva", "321");
+		
+		List<Usuario> lista = new ArrayList<>();
+		
+		lista.add(usuario1);
+		lista.add(usuario2);
+		
+		return lista;
 	}
 }
